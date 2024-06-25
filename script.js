b5058918e465e8bae4e03b766f0d8ba60721ae00
Arshadul-Monir <arshadulmonir@gmail.com>
2024-06-24 20:09:31 -0400

    Add column

--- script.js
+++ script.js
@@ -28,3 +28,31 @@ function addRow()
         grid.appendChild(row);
     }
 }
+
+function addCol()
+{
+    let grid = document.getElementById("grid");
+    const rows = document.getElementsByTagName("tr");
+    let row = document.createElement("tr");
+
+    if(rows.length == 0)
+    {
+        let col = document.createElement("td");
+        col.onclick = function() {
+            this.style.backgroundColor = color_selected
+        }
+        row.appendChild(col);
+        grid.appendChild(row);
+    }
+    else
+    {
+        for(let i = 0; i < rows.length; i++)
+        {
+            let col = document.createElement("td");
+            col.onclick = function() {
+                this.style.backgroundColor = color_selected
+            }
+            rows[i].appendChild(col);
+        }
+    }
+}
