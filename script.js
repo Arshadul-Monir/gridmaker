da5b79090edcbfed1362c136fa9dd1b599647cfc
Arshadul-Monir <arshadulmonir@gmail.com>
2024-06-24 20:06:07 -0400

    Add row

---
+++ script.js
@@ -0,0 +1,30 @@
+let color_selected;
+
+function addRow()
+{
+    let grid = document.getElementById("grid");
+    const rows = document.getElementsByTagName("tr");
+    let row = document.createElement("tr");
+
+    if(rows.length == 0)
+    {
+        let col = document.createElement("td");
+        col.onclick = function() {
+            this.style.backgroundColor = color_selected
+        }
+        row.appendChild(col);
+        grid.appendChild(row);
+    }
+    else
+    {
+        for(let i = 0; i < rows[0].childElementCount; i++)
+        {
+            let col = document.createElement("td");
+            col.onclick = function() {
+                this.style.backgroundColor = color_selected
+            }
+            row.appendChild(col);
+        }
+        grid.appendChild(row);
+    }
+}
